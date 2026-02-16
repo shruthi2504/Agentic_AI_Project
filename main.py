ad5561e0b6d2ea772c5281695bc52e2e575175e2
shruthi2504 <shruthivatsavayi25@gmail.com>
2026-02-16 08:58:10 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from orchestrator import run_orchestrator
-
-while True:
-    user_input = input("Caregiver: ")
-    if user_input.lower() in ["exit", "quit"]:
-        break
-
-    response = run_orchestrator(user_input)
-    print("\nAssistant:", response)
